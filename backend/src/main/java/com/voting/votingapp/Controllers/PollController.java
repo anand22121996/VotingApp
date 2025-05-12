a3dbdc401e21f369082fe9a854a3bd3a6a62e9f8
Anand Kumar <44093517+anand22121996@users.noreply.github.com>
2025-05-12 17:22:43 +0530

    First commit

---
+++ backend/src/main/java/com/voting/votingapp/Controllers/PollController.java
@@ -0,0 +1,53 @@
+package com.voting.votingapp.Controllers;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.voting.votingapp.model.Poll;
+import com.voting.votingapp.request.Vote;
+import com.voting.votingapp.services.PollService;
+import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+
+@RestController
+@RequestMapping("/api/polls")
+@CrossOrigin(origins = "http://localhost:4200/")
+public class PollController {
+
+    private final PollService pollService;
+
+    private PollController(PollService pollService)  {
+        this.pollService = pollService;
+    }
+
+    @PostMapping()
+    public Poll createPoll(@RequestBody Poll poll) {
+        return pollService.createPoll(poll);
+    }
+
+    @GetMapping()
+    public List<Poll> getAllPolls() {
+            return pollService.getAllPolls();
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Poll> getPoll(@PathVariable Long id) {
+
+        return pollService.getPollById(id)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
+    }
+    @PostMapping("/vote")    
+    public void vote(@RequestBody Vote vote) {
+        pollService.vote(vote.getPollId(), vote.getOptionIndex());
+    }
+    }
+    
+
---
+++ backend/src/main/java/com/voting/votingapp/services/PollService.java
@@ -0,0 +1,46 @@
+package com.voting.votingapp.services;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+
+import com.voting.votingapp.model.OptionVote;
+import com.voting.votingapp.model.Poll;
+import com.voting.votingapp.repositories.PollRepository;
+
+@Service
+public class PollService {
+
+    private final PollRepository pollRepository;
+
+    private PollService(PollRepository pollRepository) {
+        this.pollRepository = pollRepository;
+    }
+
+    public Poll createPoll(Poll poll) {
+        System.out.println("Received Poll: " + poll);
+        return pollRepository.save(poll);
+    }
+
+    public List<Poll> getAllPolls() {
+        return pollRepository.findAll();
+    }
+
+    public Optional<Poll> getPollById(Long id) {
+        return pollRepository.findById(id);
+    }
+
+    public void vote(Long pollId, int optionIndex) {
+         Poll poll = pollRepository.findById(pollId)
+                     .orElseThrow(() -> new RuntimeException("Poll Not Found"));
+
+        List <OptionVote> options = poll.getOptions();
+        if(optionIndex < 0 || optionIndex >= options.size()) {
+            throw new IllegalArgumentException("Invalid option Index");
+        }
+
+        OptionVote selectedOption = options.get(optionIndex);
+        selectedOption.setVoteCount(selectedOption.getVoteCount() + 1);
+
+        pollRepository.save(poll);
+    }
+}
